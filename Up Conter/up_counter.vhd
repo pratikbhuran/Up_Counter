7dd9bacfb62f5fb811c930993fb96b2eea124f0c
Pratik Bhuran <35888564+pratikbhuran@users.noreply.github.com>
2020-05-22 11:18:26 +0530

    Update and rename up_counter.vhd to Up Conter/up_counter.vhd

--- Up Conter/up_counter.vhd
+++ Up Conter/up_counter.vhd
@@ -13,11 +13,11 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY Up_counter IS
-GENERIC( g_COUNT_BITS	:INTEGER := 4									-- change the number of bits for up counter
+GENERIC( g_COUNT_BITS	:INTEGER := 4					-- change the number of bits for up counter
 	);
 
-PORT(	clk			:IN STD_LOGIC;										-- clock input
-	rst			:IN STD_LOGIC;										-- reset bit 
+PORT(	clk			:IN STD_LOGIC;						-- clock input
+	rst			:IN STD_LOGIC;						-- reset bit 
 	Count_o			:OUT STD_LOGIC_VECTOR(g_COUNT_BITS - 1 DOWNTO 0)	-- counter output
 	);
 END Up_counter;
@@ -30,17 +30,17 @@ SIGNAL s_count		: INTEGER RANGE 0 TO COUNT_MAX - 1 := 0;
 BEGIN
 Count_o <= STD_LOGIC_VECTOR(TO_UNSIGNED(s_count, Count_o'LENGTH));		-- assign signal value to output as output cannot be read
 
-	PROCESS( clk, rst, COUNT_MAX, s_count)								-- sensitivity list of process
+	PROCESS( clk, rst, COUNT_MAX, s_count)					-- sensitivity list of process
 		BEGIN
 			
-			IF (rst = '0') THEN											-- when reset condition is asserted
-				s_count <= (0 * s_count);									-- clear the output
+			IF (rst = '0') THEN					-- when reset condition is asserted
+				s_count <= (0 * s_count);			-- clear the output
 				
-			ELSIF(clk'EVENT AND clk ='1') THEN								-- on rising edge of clock
-				IF(s_count = COUNT_MAX - 1) THEN							-- when count reaches maximum value 
-					s_count <= (0 * s_count);								-- reset count
+			ELSIF(clk'EVENT AND clk ='1') THEN			-- on rising edge of clock
+				IF(s_count = COUNT_MAX - 1) THEN		-- when count reaches maximum value 
+					s_count <= (0 * s_count);		-- reset count
 				ELSE
-					s_count <= s_count + 1;									-- increment count by 1 when not equal to max value
+					s_count <= s_count + 1;			-- increment count by 1 when not equal to max value
 				END IF;
 			END IF;
 		
